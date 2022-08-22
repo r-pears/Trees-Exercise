55820396b54bc3bc134b7464f446bc26e2f59b62
Ryan <ryjen.pears@gmail.com>
2022-08-22 21:52:18 +0200

    binary tree class methods added

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,84 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (!this.root) return 0;
 
+    function minDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return minDepthHelper(node.right) + 1;
+      if (node.right === null) return minDepthHelper(node.left) + 1;
+
+      return (
+        Math.min(minDepthHelper(node.left), minDepthHelper(node.right)) + 1
+      );
+    }
+
+    return minDepthHelper(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
 
+    function maxDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return maxDepthHelper(node.right) + 1;
+      if (node.right === null) return maxDepthHelper(node.left) + 1;
+
+      return (
+        Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right)) + 1
+      );
+    }
+
+    return maxDepthHelper(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let result = 0;
 
+    function maxSumHelper(node) {
+      if (node === null) return 0;
+
+      let leftSum = maxSumHelper(node.left);
+      let rightSum = maxSumHelper(node.right);
+
+      result = Math.max(result, node.val + leftSum + rightSum);
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+
+    maxSumHelper(this.root);
+    return result;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
 
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.rigth);
+    }
+
+    return closest;
   }
 
   /** Further study!
@@ -46,29 +102,101 @@ class BinaryTree {
    * (i.e. are at the same level but have different parents. ) */
 
   areCousins(node1, node2) {
+    if (node1 === this.root || node2 === this.root) return false;
 
+    function findLevelAndParent(
+      nodeToFind,
+      currentNode,
+      level = 0,
+      data = { level: 0, parent: null }
+    ) {
+      if (data.parent) return data;
+
+      if (currentNode.left === nodeToFind || currentNode.right === nodeToFind) {
+        data.level = level + 1;
+        data.parent = currentNode;
+      }
+
+      if (currentNode.left) {
+        findLevelAndParent(nodeToFind, currentNode.left, level + 1, data);
+      }
+
+      if (currentNode.right) {
+        findLevelAndParent(nodeToFind, currentNode.right, level + 1, data);
+      }
+
+      return data;
+    }
+
+    let node1Info = findLevelAndParent(node1, this.root);
+    let node2Info = findLevelAndParent(node2, this.root);
+
+    let sameLevel = node1Info && node2Info && node1Info.level === node2Info.level;
+    let differentParents = node1Info && node2Info && node1Info.parent !== node2Info.parent;
+
+    return sameLevel && differentParents;
   }
 
   /** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
 
   static serialize() {
+    const values = [];
 
+    function traverse(node) {
+      if (node) {
+        values.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+      } else {
+        values.push('#');
+      }
+    }
+
+    traverse(tree.root);
+    return values.join(' ');
   }
 
   /** Further study!
    * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
   static deserialize() {
+    if (!stringTree) return null;
 
+    const values = stringTree.split(' ');
+
+    function buildTree() {
+      if (values.length) {
+        let currentVal = values.shift();
+
+        if (currentVal === '#') return null;
+
+        let currentNode = new BinaryTreeNode(+currentVal);
+        currentNode.left = buildTree();
+        currentNode.right = buildTree();
+
+        return currentNode;
+      }
+    }
+
+    const root = buildTree();
+    return new BinaryTree(root);
   }
 
   /** Further study!
    * lowestCommonAncestor(node1, node2): find the lowest common ancestor
    * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {
-    
+  lowestCommonAncestor(node1, node2, currentNode = this.root) {
+    if (currentNode === null) return null;
+    if (currentNode === node1 || currentNode === node2) return currentNode;
+
+    let left = this.lowestCommonAncestor(node1, node2, currentNode.left);
+    let right = this.lowestCommonAncestor(node1, node2, currentNode.right);
+
+    if (left !== null && right !== null) return currentNode;
+    if (left !== null || right !== null) return left || right;
+    if (left === null && right === null) return null;
   }
 }
 
